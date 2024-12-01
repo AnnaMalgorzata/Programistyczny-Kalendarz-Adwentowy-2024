73bda7043213d9d047bc35fa6faee1af0e4186d6
Anna Godek <anna.m.godek@gmail.com>
2024-12-01 22:33:48 +0100

    Pierwszy commit

---
+++ 1_DrawingChristmasTree/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.WriteLine("Podaj wysokość choinki:");
+string? input = Console.ReadLine();
+
+if (!int.TryParse(input, out int wysokosc) || wysokosc <= 1)
+{
+    Console.WriteLine("Niepoprawna wysokość.");
+    return;
+}
+
+int szerokosc = wysokosc * 2 - 1;
+string[,] tree = new string[wysokosc + 1, szerokosc];
+
+for (int i = 0; i < wysokosc + 1; i++)
+{
+    for (int j = 0; j < szerokosc; j++)
+    {
+        tree[i, j] = " ";
+    }
+}
+
+for (int i = 0; i < wysokosc; i++)
+{
+    for (int j = szerokosc / 2 - i; j <= szerokosc / 2 + i; j++)
+    {
+        tree[i, j] = "*";
+    }
+}
+tree[wysokosc, wysokosc - 1] = "||";
+
+for (int i = 0; i < wysokosc + 1; i++)
+{
+    for (int j = 0; j < szerokosc; j++)
+    {
+        Console.Write(tree[i, j]);
+    }
+    Console.WriteLine();
+}
