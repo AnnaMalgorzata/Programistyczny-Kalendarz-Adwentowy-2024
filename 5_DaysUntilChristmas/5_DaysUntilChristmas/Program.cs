8f6ce1bad0efa517e35d4fd8bc47179f554d43e9
Anna Godek <anna.m.godek@gmail.com>
2024-12-05 22:20:39 +0100

    Day 5 - days until Christmas

---
+++ 5_DaysUntilChristmas/5_DaysUntilChristmas/Program.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * Policz ile dni zostało do Świąt Bożego Narodzenia i wyświetl na ekranie. Jeśli aktualna
+ * data będzie po tegorocznych Świętach, policz liczbę dni do najbliższego Bożego Narodzenia :)
+ */
+
+const int ChristmasMonth = 12;
+const int ChristmasDay = 25;
+
+DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+DateOnly christmas = new DateOnly(today.Year, ChristmasMonth, ChristmasDay);
+
+if (today > christmas)
+{
+    christmas = new DateOnly(today.Year + 1, ChristmasMonth, ChristmasDay);
+}
+
+var daysTillChristmas = (christmas.DayNumber - today.DayNumber);
+
+Console.WriteLine($"Dni do najbliższych świąt: {daysTillChristmas}");
+
