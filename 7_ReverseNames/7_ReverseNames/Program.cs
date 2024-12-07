33a6769c5d39979c9216563966080a11477d84c8
Anna Godek <anna.m.godek@gmail.com>
2024-12-07 23:07:13 +0100

    Day 7 - reverse names

---
+++ 7_ReverseNames/7_ReverseNames/Program.cs
@@ -0,0 +1,12 @@
+﻿/**
+ * W systemie Mikołaja wystąpił błąd i wszystkie imiona dzieci zostały odwrócone.
+ * Napisz program, który odwróci imiona oraz wypisze je w konsoli dużymi literami.
+ */
+
+List<string> names = new List<string> { "alop", "nicram", "ketjow", "mada", "kenaj" };
+
+for (int i = 0; i < names.Count; i++)
+{
+    names[i] = new string(names[i].Reverse().ToArray());
+    Console.WriteLine(names[i].ToUpper());
+}
