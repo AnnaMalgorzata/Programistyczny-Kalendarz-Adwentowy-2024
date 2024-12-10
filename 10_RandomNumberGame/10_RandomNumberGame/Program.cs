f3cbb558a67366645ffc36e5fe5925ddf1c7efa5
Anna Godek <anna.m.godek@gmail.com>
2024-12-10 17:22:56 +0100

    Day 10 - guessing random number

---
+++ 10_RandomNumberGame/10_RandomNumberGame/Program.cs
@@ -0,0 +1,34 @@
+﻿Random random = new Random();
+int secretNumber = random.Next(0, 100); 
+int userGuess = -1;
+int userGuessesNumber = 0;
+
+
+Console.WriteLine("Zgadnij liczbę z przedziału 0 - 99.");
+
+while (userGuess != secretNumber)
+{
+    Console.Write("Podaj swoją liczbę: ");
+    string input = Console.ReadLine();
+
+    if (!int.TryParse(input, out userGuess))
+    {
+        Console.WriteLine("To nie jest poprawna liczba. Spróbuj ponownie.");
+        continue;
+    }
+
+    if (userGuess < secretNumber)
+    {
+        Console.WriteLine("Za mało! Spróbuj jeszcze raz.");
+        userGuessesNumber++;
+    }
+    else if (userGuess > secretNumber)
+    {
+        Console.WriteLine("Za dużo! Spróbuj jeszcze raz.");
+        userGuessesNumber++;
+    }
+    else
+    {
+        Console.WriteLine($"Gratulacje! Zgadłeś liczbę {secretNumber} za {userGuessesNumber} razem");
+    }
+}
